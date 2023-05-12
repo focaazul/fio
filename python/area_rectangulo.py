6163089c561a79918d502143059e37dbe735ec30
focaazul <49849051+focaazul@users.noreply.github.com>
2023-05-12 09:54:31 -0300

    Create area_rectangulo.py

---
+++ python/area_rectangulo.py
@@ -0,0 +1,7 @@
+def area_rectangulo(base, altura):
+    return base*altura
+
+#Funcion principal
+altura=int(input("Ingrese Altura: "))
+base=int(input("Ingrese Base: "))
+print( area_rectangulo(base,altura) )#Llamo a la función dentro del print
