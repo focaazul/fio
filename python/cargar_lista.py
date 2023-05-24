a0f6ee25c446b60f5543e15dce641c5d3c8d95c6
focaazul <49849051+focaazul@users.noreply.github.com>
2023-05-24 15:00:14 -0300

    Update cargar_lista.py

--- python/cargar_lista.py
+++ python/cargar_lista.py
@@ -1,5 +1,5 @@
 "Cargar una lista de 5 elementos con valores reales y string"
-#Aternativa 1
+#Aternativa 1 , usando try/except
 valores=[]
 for i in range(5):
     dato=input("Ingrese dato: ")
@@ -10,16 +10,15 @@ for i in range(5):
         pass
     valores.append(dato)
 for j in valores:
-    print(j,"-",type(j))
+    print(j,"-",type(j))#para mostrar valores y tipos cargados en la lista
 
-#Alternativa 2
+#Alternativa 2 , usando un método de string, llamado isdecimal()
 valores1=[]
 for i in range(5):
     dato=input("Ingrese dato: ")
-    print("valor",dato.isalnum())
-    if dato.isdecimal(): #si solo hay nrumeros 
+    if dato.isdecimal(): #si solo hay numeros esto es True
         valores1.append(float(dato)) # convierte string a float.
     else:
         valores1.append(dato)
 for j in valores1:
-    print(j,"-",type(j))
+    print(j,"-",type(j))#para mostrar valores y tipos cargados en la lista
