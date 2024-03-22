2ddaf2231cc4274e60ecfb5633b29dac5c8f04b5
focaazul <49849051+focaazul@users.noreply.github.com>
2024-03-22 15:36:55 -0300

    Update exception3.cpp

--- cpp/exception3.cpp
+++ cpp/exception3.cpp
@@ -1,4 +1,3 @@
-// Demostración: try, throw y catch
 
 #include <iostream>
 #include <cmath>
@@ -11,42 +10,45 @@ static const int ERANGO=101;
 class ErrorMat
 {
 public:
-    ErrorMat() : motivo(0) {};
-    ErrorMat(int m) : motivo(m) {};
-    const char* porque() const throw();
-
+	ErrorMat() : motivo(0) {};
+	ErrorMat(int m) : motivo(m) {};
+	const char* porque() const throw();
+	
 private:
-    int motivo;
+	int motivo;
 };
 
 const char* ErrorMat::porque() const throw()
 {
-    switch (motivo)
-    {
-    case EDOMINIO: return "Error de Dominio ";break;
-    case ERANGO:   return "Error de Rango ";break;
-    default:       return "Error Desconocido";  //En rigor no debería ocurrir tal Excepción 
-    }	
+	switch (motivo)
+	{
+	case EDOMINIO: return "Error, no se puede calcular logaritmo de valor negativo ";break;
+	case ERANGO:   return "Error, no existe el logaritmo de cero ";break;
+	default:       return "Error Desconocido";  //En rigor no debería ocurrir tal Excepción 
+	}	
 }
 
-double logaritmo(const double n)
+double logaritmo(const double n) //Función logaritmo.
 {
-    try {
-        if (n < 0) throw(ErrorMat(EDOMINIO) );
-        if (n == 0) throw(ErrorMat(ERANGO) );
-        return log(n);
+	try {//creo el bloque try para trata si existe  exception
+		if (n < 0) throw(ErrorMat(EDOMINIO) ); //si es negativo invoco exception con 
+												// argumento clase ErrorMat , argumeto EDOMINIO
+		if (n == 0) throw(ErrorMat(ERANGO) );//si es cero invoco exception con 
+											// argumento clase ErrorMat , argumeto ERANGO
+		return log(n);// si no hay excepciones calculo el log(n)
 	}
-    catch(ErrorMat& e) {
-        cout << e.porque();
-    }
-    return 0;
+	catch(ErrorMat& argumento) {//catch captura invocando a la clase ErrorMat y le pasa el argumento.
+		cout << argumento.porque();// el método porque aplicado sobre el argumento 
+							//explica el origen del error.
+	}
+	return 0;
 }
 
 int main()
 {
-    double r = 100;
-    cout << "log(" << r << ") = " << logaritmo(r) << endl;
-    cout << "log(-" << r << ") = " << logaritmo(-r) << endl;
-    cin.get();
-    return 0;
+	double r ;
+	cout<<"Ingrese el valor a calcular el log en base 10: "<<endl;
+	cin>>r;
+	cout << "log(" << r << ") = " << logaritmo(r) << endl;//invoco a logaritmo, que tiene el manejo de excepcion
+	return 0;
 }
