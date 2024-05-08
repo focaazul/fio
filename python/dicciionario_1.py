a800cd3132fe6527579af914271c2be4df87e783
focaazul <49849051+focaazul@users.noreply.github.com>
2024-05-08 14:14:47 -0300

    Create dicciionario_1.py

---
+++ python/dicciionario_1.py
@@ -0,0 +1,22 @@
+"""Consigna: Mostar el tipo de dato de cada valor del 
+Diccionario"""
+
+#Declaro diccionario.
+diccio={1:"Hola","2":True,"Lista":["a","b","c"],"D":{"a":1,"b":2}}
+#vemos que existen valores de distintos tipos
+print("Las claves son:")
+for t in diccio: #t asume los valores de las claves.abs
+    print(t)
+print("Veamos los tipos de los valores...")
+#ahora mostramos el tipo de dato de cada valor.
+for y in diccio: #t asume los valores de las claves.abs
+    if isinstance(y,list):
+        print("La clave:",y," es del tipo lista")
+    elif isinstance(y,bool):
+        print("La clave:",y," es del tipo booleano")
+    elif isinstance(y,str):
+        print("La clave:",y," es del tipo string")
+    elif isinstance(y,dict):
+        print("La clave:",y," es del tipo diccionario")
+    else:
+        print(f"La clave {y} es un tipo distinto de lista,diccionario,str o bool")
