07eaf1b64e798bd84919f4b6e90f52ba3946de04
focaazul <49849051+focaazul@users.noreply.github.com>
2024-05-24 15:08:06 -0300

    Create argumentos.cpp

---
+++ cpp/argumentos.cpp
@@ -0,0 +1,16 @@
+/*Este código asigna como nombre de un archivo al argumento al ejecutar en la terminal */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+cout<<"cantidad de argumentos pasados:"<<argc<<endl;
+for(int i=0;i<argc;i++) cout<<"Argumento :"<<i<<" es : "<<argv[i]<<endl;
+fstream flujo(argv[1], ios::app);
+flujo<<"Hola 1 "<<endl;
+flujo<<"Mundo 1 "<<endl;
+flujo.close();
+return 0;
+}
