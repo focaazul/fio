d1c9723b6222c8892a1bf6324049549ba3331bcc
focaazul <49849051+focaazul@users.noreply.github.com>
2025-03-05 12:17:51 -0300

    Create json1.py

---
+++ python/json1.py
@@ -0,0 +1,11 @@
+import json #importo la librería.
+
+#Lista con varios diccionarios como elementos de la lista
+pedidos = [
+    {"numero": 1, "cliente": "Juan Pérez", "productos": [{"nombre": "Café", "precio": 250, "cantidad": 2}], "estado": "pendiente"},
+    {"numero": 2, "cliente": "María López", "productos": [{"nombre": "Té", "precio": 200, "cantidad": 1}], "estado": "en preparación"}
+]
+
+# Guardar en un archivo JSON
+with open("pedidos.json", "w") as archivo:
+    json.dump(pedidos, archivo, indent=4)  # indent=4 para mejor legibilidad
