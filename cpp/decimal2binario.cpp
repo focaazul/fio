0ede617e51f07589755282122dcc720aa4387bc8
focaazul <49849051+focaazul@users.noreply.github.com>
2025-03-20 12:35:47 -0300

    Update decimal2binario.cpp

--- cpp/decimal2binario.cpp
+++ cpp/decimal2binario.cpp
@@ -2,14 +2,31 @@
 using namespace std;
 int main()
 {
-int decimal=12345678;
-int binario[9];
+int decimal=397;
+int binario[9];// se que 2 elevado a la 9=512 > 397
+// con 9 bit puedo representar el nro. 397 en binario.
+int temp;
 
-for(int i=0;i<9;i++){
+for(int i=0;i<10;i++){
 binario[i]=decimal%2;//tomo el resto de la división
 decimal=decimal/2;//tomo el nuevo cociente.
 }
-
+//Esto sería lo almacenado en el Arreglo
 for(int i=0; i<9;i++){
 cout<<binario[i];
+}
+cout<<endl<<" vemos que falta invertir el orden"<<endl;
+//Si verificamos veremos que esta invertido, vamos a corregir.
+//roto en el mismo arreglo,
+for(int i=0; i<5;i++){
+temp=binario[i];
+binario[i]=binario[8-i];//roto los valores
+binario[8-i]=temp;
+}
+//Esto sería lo almacenado en el Arreglo
+
+for(int i=0; i<10;i++){
+cout<<binario[i];
+}
+cout<<endl<<" luego de invertir el orden .."<<endl;
 }
