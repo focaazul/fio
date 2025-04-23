000c56c52976735556f94f508ffc6abd05409dfd
focaazul <49849051+focaazul@users.noreply.github.com>
2025-04-23 16:52:09 -0300

    Update arreglo_de_punteros1.cpp

--- cpp/arreglo_de_punteros1.cpp
+++ cpp/arreglo_de_punteros1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char *argv[]) {
-    float *pa;//declaro puntero a float
+    float *pa,*pi;//declaro puntero a float
     cout<<sizeof(float)<<endl;//vero el tamaño de float en bytes
-    pa=new float[5];//con new reservo 5 espacios consecutivos de memoria
+    pi=pa=new float[5];//con new reservo 5 espacios consecutivos de memoria
                     // para almacenar direcciones de memoria
     //obtengo una dirección en pa, puntero al primer elemento del arreglo!!
-    for(int i=0;i<5;i++)cin>>pa[i];//cargo valores pero usando punteros !!
+    for(int i=0;i<5;i++){cin>>*pa;pa++;}//cargo valores pero usando punteros !!
+    pa=pi; //para regresar a la posición 0 del arreglo
     for(int i=0;i<5;i++)cout<<pa[i]<<"\t"<<&pa[i]<<endl;//muestro lo almacenado
     return 0;
 }
