e9ac7a5e710a9fed1b34774f74fba7c94626137b
focaazul <49849051+focaazul@users.noreply.github.com>
2025-04-23 15:58:18 -0300

    Update arreglo_de_punteros3.cpp

--- cpp/arreglo_de_punteros3.cpp
+++ cpp/arreglo_de_punteros3.cpp
@@ -5,14 +5,18 @@ int main(int argc, char *argv[]){
     px=new float[5];//Declaro arreglo de punteros CONSECUTIVOS
     float *pa[5];//Declaro arreglo de punteros <-Esto es nuevo!!
     cout<<sizeof(float)<<endl;
+    //Cargo --->pa
     //Espacios NO consecutivos ( cada vez que ejecuto new) .
     for(int i=0;i<5;i++){
         pa[i]=new float;//dinámicamente resevo UN espacio en memoria.
         cin>>*pa[i];//cada puntero ahora apunta al dato ingresado:inicializo
     }
+     //Muestro--->pa
     for(int i=0;i<5;i++)cout<<*pa[i]<<" "<<pa[i]<<endl;//Espacios consecutivos.
+      //Cargo --->px
     for(int i=0;i<5;i++)
         cin>>*(px+i);//cada puntero ahora apunta al dato ingresado
+    //Muestro--->px
     for(int i=0;i<5;i++)cout<<*(px+i)<<" "<<(px+i)<<endl;
     return 0;
 }
